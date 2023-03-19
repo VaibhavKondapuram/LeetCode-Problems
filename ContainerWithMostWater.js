302c5837c7157d9ddca1f9c6493622cf362f4c8a
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-03-19 17:09:15 +0530

    done

---
+++ ContainerWithMostWater.js
@@ -0,0 +1,20 @@
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        let width = right - left;
+
+        maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * width);
+
+        if (height[left] <= height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+    
+};
