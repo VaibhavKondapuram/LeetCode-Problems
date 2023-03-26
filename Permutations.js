fd7fc5863f81199214e673b42a1652595c595727
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-03-26 11:56:21 +0530

    done

---
+++ Permutations.js
@@ -0,0 +1,15 @@
+var permute = function(nums) {
+    let res = []
+
+    let iterate = (arr,temp) =>{
+        if(arr.length == 0){
+            res.push(temp)
+            return;
+        }
+        for(let i =0;i<arr.length;i++){
+            iterate(arr.filter((num,idx)=>idx !=i),[...temp,arr[i]])
+        }
+    }
+    iterate(nums,[])
+    return res
+};
