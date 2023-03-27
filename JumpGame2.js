b71da24fda1dd28018bc475ea44a4b7a05a7c483
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-03-27 14:21:30 +0530

    done

---
+++ JumpGame2.js
@@ -0,0 +1,13 @@
+var jump = function(nums) {
+    var output = 0
+    var jump = 0
+    var far = 0
+    for(i=0;i<nums.length-1;i++){
+        jump = Math.max(jump , nums[i] + i)
+        if(i == far){
+            far = jump
+            output ++
+        }
+    }
+    return output
+};
